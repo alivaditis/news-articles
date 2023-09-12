39ca51053f774d5029f4b92297e91b7f6c32ec4a
Alec Livaditis <aleclivaditis@gmail.com>
2023-09-12 11:37:28 -0400

    add routes

--- src/api.js
+++ src/api.js
@@ -1,19 +1,20 @@
 const apiKey = process.env.REACT_APP_API_KEY
 
-const getArticlesByCategory = (category, source) => {
-  let url
+const getArticles = (category, source) => {
+  let url = `https://newsapi.org/v2/top-headlines?apiKey=${apiKey}&country=us`
   if (category) {
-    url = `https://newsapi.org/v2/top-headlines?apiKey=${apiKey}&category=${category}`
+    url += `&category=${category}`
   } 
   if (source) {
-    url = `https://newsapi.org/v2/top-headlines?apiKey=${apiKey}&sources=${source}`
+    url += `&sources=${source}`
   }
   return fetch(url)
     .then(res => {
       if (!res.ok) {
         throw new (`HTTP Error: ${res.status} /n We could not load the content you are looking for`)
-      } else {
-        return res.json
       }
+      return res.json()
     })
 }
+
+export { getArticles }
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { getArticles } from '../../api';
+import { Routes, Route } from 'react-router-dom'
+import Landing from '../Landing/Landing'
 import './App.css';
+import Article from '../Article/Article';
 
 function App() {
+  const [topArticles, setTopArticles] = useState([])
+
+  useEffect(() => {
+    getArticles()
+      .then(res => setTopArticles(res.articles.slice(0, 5)))
+      .catch(err => console.log(err))
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {topArticles.length > 0 && topArticles.map(article => <p>{article.title}</p>)}
+      <Routes>
+        <Route path='/' element={<Landing/>}/>
+        <Route path='/categories/:category' element={<Landing/>}/>
+        <Route path='/articles/:id' element={<Article/>}/>
+      </Routes>
     </div>
   );
 }
