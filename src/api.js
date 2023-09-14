bcc67bf2afad3def681daa582340270a2000b4b7
Alec Livaditis <aleclivaditis@gmail.com>
2023-09-13 21:41:45 -0400

    refactor fetch

--- src/api.js
+++ src/api.js
@@ -1,14 +1,18 @@
 const apiKey = process.env.REACT_APP_API_KEY
 
 const getArticles = (category, source) => {
-  let url = `https://newsapi.org/v2/top-headlines?apiKey=${apiKey}&country=us`
+  let url = `https://newsapi.org/v2/top-headlines?country=us`
   if (category) {
     url += `&category=${category}`
   } 
   if (source) {
     url += `&sources=${source}`
   }
-  return fetch(url)
+  return fetch(url, {
+    headers: {
+      'x-api-key': apiKey
+    }
+  })
     .then(res => {
       if (!res.ok) {
         throw new (`HTTP Error: ${res.status} /n We could not load the content you are looking for`)
